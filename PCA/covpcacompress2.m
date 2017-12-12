47995c309fb8b897cda4dc35e9a009dc1edab1b3
Nothing <yjh19950805@163.com>
2017-12-12 22:48:06 +0800

    同步

--- PCA/covpcacompress2.m
+++ PCA/covpcacompress2.m
@@ -3,15 +3,18 @@ clear;
 %%读取图片，产生样本
 Xp = zeros(112*92,10);
 In = zeros(112,92,10);
+for j = 1:40
+    picdir = ['face/s',num2str(j),'/'];
+    outdir = ['compress2/s',num2str(j),'/'];
 for i = 1:10
-    picpath = ['face/s3/',num2str(i),'.bmp']
+    picpath = [picdir,num2str(i),'.bmp'];
     pic = imread(picpath);
     pic = im2double(pic);
     In(:,:,i) = pic;
     pic = reshape(pic,112*92,1);
     Xp(:,i) = pic;
 end
-[Xh,v] = nothingsccipca(Xp,5);
+[Xh,v] = nothingsccipca(Xp,7);
 
 Out = zeros(112,92,10);
 for i = 1:10
@@ -19,10 +22,15 @@ pic = Xh(:,i);
 pic = reshape(pic,112,92);
 Out(:,:,i) = pic;
 pic = im2uint8(pic);
-picpath = ['compress1/s3/',num2str(i),'.bmp']
+if ~exist(outdir)
+    mkdir(outdir)
+end
+picpath = [outdir,num2str(i),'.bmp'];
 imwrite(pic,picpath);
 end
 for i = 1:4
     subplot(2,4,2*i-1);imshow(In(:,:,i));
     subplot(2,4,2*i);imshow(Out(:,:,i));
+end
+psnr = PSNR(Xp,Xh)
 end
