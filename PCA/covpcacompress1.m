1d08432d64b5877c5957ec974e6d56215a78696f
Nothing <yjh19950805@163.com>
2017-12-13 09:22:34 +0800

    同步

--- PCA/covpcacompress1.m
+++ PCA/covpcacompress1.m
@@ -3,15 +3,18 @@ clear;
 %%读取图片，产生样本
 Xp = zeros(10,112*92);
 In = zeros(112,92,10);
+for j = 1:40
+    picdir = ['face/s',num2str(j),'/'];
+    outdir = ['compress1/s',num2str(j),'/'];
 for i = 1:10
-    picpath = ['face/s3/',num2str(i),'.bmp']
+    picpath = [picdir,num2str(i),'.bmp'];
     pic = imread(picpath);
     pic = im2double(pic);
     In(:,:,i) = pic;
     pic = reshape(pic,1,112*92);
     Xp(i,:) = pic;
 end
-[Xh,Yh,u] = nothingsconpca(Xp,6);
+[Xh,Yh,u] = nothingsconpca(Xp,8);
 
 Out = zeros(112,92,10);
 
@@ -20,13 +23,15 @@ pic = Xh(i,:);
 pic = reshape(pic,112,92);
 Out(:,:,i) = pic;
 pic = im2uint8(pic);
-if ~exist('compress1/s3/')
-    mkdir('compress1/s3/')
+if ~exist(outdir)
+    mkdir(outdir)
 end
-picpath = ['compress1/s3/',num2str(i),'.bmp']
+picpath = [outdir,num2str(i),'.bmp']
 imwrite(pic,picpath);
 end
 for i = 1:4
     subplot(2,4,2*i-1);imshow(In(:,:,i));
     subplot(2,4,2*i);imshow(Out(:,:,i));
+end
+psnr=PSNR(Xp,Xh)
 end
