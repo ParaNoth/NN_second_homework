c4c48a0340767bf046cc30f262673c1c9710741c
Nothing <yjh19950805@163.com>
2017-12-17 03:47:53 +0800

    同步

--- ICA/mixpic.m
+++ ICA/mixpic.m
@@ -2,9 +2,9 @@ clc;
 clear;
 rawpic = [];
 path1 = '�羰ͼ/';
-outpath = 'result/3-3/hj/';
-%picnum = randperm(4);
-picnum = [1,2,4];
+outpath = 'result/4-3/hj/';
+picnum = randperm(4);
+%picnum = [1,2,4];
 for i = 1:3
     picpath = [path1,num2str(picnum(i)),'.bmp'];
     pic = imread(picpath);
@@ -12,7 +12,8 @@ for i = 1:3
     pic = pic(:)';
     rawpic = [rawpic;pic];
 end
-mixmatrix = [0.3 0.2 0.5;0.7 0.2 0.1;0.2 0.5 0.2];
+mixmatrix = rand(4,3);
+mixmatrix = mixmatrix./sum(mixmatrix);
 picmix = mixmatrix*rawpic;
 
 
