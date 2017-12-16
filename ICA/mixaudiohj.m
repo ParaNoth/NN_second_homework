f7371fe15914ca86af53f0f7af0825724191e659
Nothing <yjh19950805@163.com>
2017-12-17 04:12:16 +0800

    同步

---
+++ ICA/mixaudiohj.m
@@ -0,0 +1,38 @@
+clc;
+clear;
+audiobuffer = [];
+path1 = 'voice/';
+outpath = 'result/audio3-3/hj/';
+
+aa = dir(path1);
+[n,~]=size(aa);
+%audionum = randperm(n-2);
+audionum = [1,2,3];
+filelist = {};
+for i = 1:n-2
+    filelist{i}=aa(i+2).name;
+end
+
+%picnum = [1 2 3];
+FSbuffer = zeros(3,1);
+for i = 1:3
+    audiopath = [path1,filelist{audionum(i)}];
+    [audio,FS] = audioread(audiopath);
+    FSbuffer(i) = FS;
+    audiobuffer = [audiobuffer;audio'];
+end
+mixmatrix = rand(4,3);
+mixmatrix = mixmatrix./sum(mixmatrix);
+audiomix = mixmatrix*audiobuffer;
+
+
+
+s = nothingshjica(audiomix);
+for i = 1:3
+    if ~exist(outpath)
+        mkdir(outpath);
+    end
+    audiopath = [outpath,num2str(i),'.wav'];
+    audio = s(i,:)';
+    audiowrite(audiopath,audio,FS);
+end
